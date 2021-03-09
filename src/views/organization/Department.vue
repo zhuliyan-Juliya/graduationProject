7cb584de3ce981ba014c00c7f60bc41a946da7c4
朱丽燕 <3415354652@qq.com>
2021-03-09 20:55:43 +0800

    update 部门设置页面

--- src/views/organization/Department.vue
+++ src/views/organization/Department.vue
@@ -2,28 +2,33 @@
   <div>
     <el-card shadow="hover">
       <el-row style="padding-bottom: 15px">
-        <el-button type="primary" icon="el-icon-plus" @click="newCompany">新增</el-button>
+        <el-button type="primary" icon="el-icon-plus" @click="newDepartment()">新增</el-button>
         <el-button icon="el-icon-upload">导入</el-button>
         <el-button>子部门排序</el-button>
       </el-row>
 
-      <el-table :data="tableData" ref="multipleTable" style="width: 100%">
+      <el-table :data="tableData" ref="multipleTable" style="width: 100%" v-loading="tableLoading">
         <el-table-column type="selection" width="55"> </el-table-column>
-        <el-table-column prop="id" label="ID" width="150"> </el-table-column>
-        <el-table-column prop="deptName" label="部门"> </el-table-column>
+        <el-table-column prop="name" label="部门"> </el-table-column>
         <el-table-column prop="short_name" label="部门简称"> </el-table-column>
         <el-table-column prop="parent_name" label="所属公司/中心">
           <template slot-scope="scope">
             <div>
-              {{ scope.row.sub_company.name }}
+              {{ scope.row.company ? scope.row.company.name : '' }}
             </div>
           </template>
         </el-table-column>
-        <el-table-column prop="people_num" label="上级部门"> </el-table-column>
+        <el-table-column prop="parent" label="上级部门">
+          <template slot-scope="scope">
+            <div>
+              {{ scope.row.parent ? scope.row.parent.name : '' }}
+            </div>
+          </template>
+        </el-table-column>
         <el-table-column prop="people_num" label="部门负责人">
           <template slot-scope="scope">
             <div>
-              {{ scope.row.dep_head.fullName }}
+              {{ scope.row.dep_head ? scope.row.dep_head.fullName : '' }}
             </div>
           </template>
         </el-table-column>
@@ -31,7 +36,7 @@
         <el-table-column prop="people_num" label="工作城市">
           <template slot-scope="scope">
             <div>
-              {{ scope.row.work_city ? scope.row.work_city : '不指定城市' }}
+              {{ scope.row.city ? scope.row.city.region_name : '不指定城市' }}
             </div>
           </template>
         </el-table-column>
@@ -40,15 +45,25 @@
         <el-table-column prop="people_num" label="超缺编"> </el-table-column>
         <el-table-column prop="status" label="状态">
           <template slot-scope="scope">
-            <p v-if="scope.row.status == 0">禁用</p>
-            <p v-else-if="scope.row.status == 1">启用</p>
+            <p v-if="scope.row.status === 0">
+              <span class="forbidden">
+                <span>禁用</span>
+              </span>
+            </p>
+            <p v-else>
+              <span class="startUse">
+                <span>启用</span>
+              </span>
+            </p>
           </template>
         </el-table-column>
         <el-table-column fixed="right" label="操作" min-width="120">
           <template slot-scope="scope">
-            <el-button @click="newCompany(scope.row)" type="text" size="small">编辑</el-button>
-            <el-button type="text" size="small">禁用</el-button>
-            <el-button type="text" size="small" style="color: #ff8c00">删除</el-button>
+            <el-button @click="newDepartment(scope.row)" type="text" size="small">编辑</el-button>
+            <el-button type="text" size="small" @click="toggleStatus(scope.row)">
+              {{ scope.row.status === 0 ? '启用' : '禁用' }}
+            </el-button>
+            <el-button type="text" size="small" style="color: #ff8c00" @click="deleteDepartment(scope.row._id)">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -58,33 +73,33 @@
       <el-form :value="itemInfo" class="wid-100" inline label-position="right" label-width="120px">
         <el-row>
           <el-col :span="12">
-            <el-form-item label="名称：">
+            <el-form-item label="部门名称：">
               <el-input v-model.trim="itemInfo.name" clearable style="width: 300px" placeholder="请输入部门名称"></el-input>
             </el-form-item>
           </el-col>
           <el-col :span="12">
-            <el-form-item label="单位简称：">
-              <el-input v-model.trim="itemInfo.rebates" clearable style="width: 300px" placeholder="请输入部门简称"></el-input>
+            <el-form-item label="部门简称：">
+              <el-input v-model.trim="itemInfo.short_name" clearable style="width: 300px" placeholder="请输入部门简称"></el-input>
             </el-form-item>
           </el-col>
           <el-col :span="12">
-            <el-form-item label="类型：">
-              <el-select v-model="itemInfo.typeText" style="width: 300px" clearable>
-                <el-option v-for="(item, index) in typeList" :key="index" :value="item.value" :label="item.label" />
+            <el-form-item label="所属公司：">
+              <el-select v-model="itemInfo.companyID" style="width: 300px" clearable>
+                <el-option v-for="(item, index) in companyList" :key="index" :value="item.value" :label="item.label" />
               </el-select>
             </el-form-item>
           </el-col>
           <el-col :span="12">
-            <el-form-item label="上级：">
-              <el-select v-model="itemInfo.parent_name" style="width: 300px" clearable>
+            <el-form-item label="上级部门：">
+              <el-select v-model="itemInfo.parentID" style="width: 300px" clearable>
                 <el-option v-for="(item, index) in parentOptions" :key="index" :value="item.value" :label="item.label" />
               </el-select>
             </el-form-item>
           </el-col>
           <el-col :span="12">
-            <el-form-item label="职位体系：">
-              <el-select v-model="itemInfo.scheme" style="width: 300px">
-                <el-option v-for="(item, index) in schemeOptions" :key="index" :disabled="item.disable" :value="item.value" :label="item.label" />
+            <el-form-item label="所在城市：">
+              <el-select v-model="itemInfo.cityID" style="width: 300px">
+                <el-option v-for="(item, index) in cityOptions" :key="index" :disabled="item.disable" :value="item.value" :label="item.label" />
               </el-select>
             </el-form-item>
           </el-col>
@@ -108,39 +123,9 @@ export default {
   data() {
     return {
       DialogFlag: false,
-      tableData: [
-        {
-          company_id: '4716',
-          created_at: '2021-02-28 17:38:22',
-          dep_head: { id: '1067233', fullName: '盛静银', profileImage: null, designation: '', mobileNumber: '18368490622' },
-          deptName: '技术部',
-          des_id: null,
-          designation: '',
-          ding_id: '0',
-          ding_parentid: '0',
-          employee_count: 0,
-          feishu_id: '0',
-          feishu_parent_id: '0',
-          id: '18433',
-          is_default: '0',
-          nums: '0',
-          order: '99999999',
-          parent: [],
-          parent_department: null,
-          parent_id: '0',
-          part_time_department_leaders: [],
-          part_time_department_name: '',
-          short_name: 'jsb',
-          status: '1',
-          sub_company: { id: 6949, company_id: 4716, pid: 0, type: 0, name: 'testCompany', status: 1 },
-          sub_id: '6949',
-          updated_at: '2021-02-28 17:38:22',
-          wework_id: '0',
-          wework_parentid: '0',
-          work_city: null,
-          work_city_id: '0',
-        },
-      ],
+      tableLoading: false,
+      companyList: [],
+      tableData: [],
       editFlag: false,
       itemInfo: {},
       sourceData: {
@@ -151,40 +136,95 @@ export default {
         { value: '分部', label: '分部' },
         { value: '中心', label: '中心' },
       ],
-      parentOptions: [{ value: 'testCompany', label: 'testCompany' }],
-      schemeOptions: [{ value: '', label: '暂无数据', disable: true }],
+      parentOptions: [],
+      cityOptions: [],
     };
   },
-  mounted() {},
+  mounted() {
+    this.getDepartmentList();
+    this.getCompanyList();
+    this.getWorkCityList();
+  },
   methods: {
-    newCompany(editContent) {
+    getCompanyList() {
+      this.$api.getCompanyList().then(res => {
+        if (res.success) {
+          this.companyList = res.data.map(item => {
+            return { label: item.name, value: item._id };
+          });
+        }
+      });
+    },
+    getWorkCityList() {
+      this.$api.getCityList().then(res => {
+        if (res.success) {
+          this.cityOptions = res.data.map(item => {
+            return { label: item.region_name, value: item._id };
+          });
+        }
+      });
+    },
+    toggleStatus(item) {
+      let obj = {
+        status: Number(!item.status),
+        id: item._id,
+      };
+      this.tableLoading = true;
+      this.$api.toggleDepartmentStatus(obj).then(res => {
+        if (res.success) {
+          this.getDepartmentList();
+        }
+      });
+    },
+    newDepartment(editContent) {
       if (!!editContent) {
         // 编辑
         this.editFlag = true;
         this.itemInfo = JSON.parse(JSON.stringify(editContent));
-        console.log('this.itemInfo', this.itemInfo);
       } else {
         this.editFlag = false;
         this.itemInfo = JSON.parse(JSON.stringify(this.sourceData));
       }
       this.DialogFlag = true;
     },
+    deleteDepartment(id) {
+      this.tableLoading = true;
+      this.$api.deleteDepartment({ id: id }).then(res => {
+        if (res.success) {
+          this.getDepartmentList();
+        }
+      });
+    },
     passCancel() {
       this.DialogFlag = false;
-      this.itemInfo = {};
     },
-    passConfirm() {},
-    toggleSelection(rows) {
-      if (rows) {
-        rows.forEach(row => {
-          this.$refs.multipleTable.toggleRowSelection(row);
+    passConfirm() {
+      this.DialogFlag = false;
+      if (this.editFlag) {
+        this.$api.editDepartment(this.itemInfo).then(res => {
+          if (res.success) {
+            this.getDepartmentList();
+          }
         });
       } else {
-        this.$refs.multipleTable.clearSelection();
+        this.$api.creatDepartment(this.itemInfo).then(res => {
+          if (res.success) {
+            this.getDepartmentList();
+          }
+        });
       }
     },
-    handleSelectionChange(val) {
-      this.multipleSelection = val;
+    getDepartmentList() {
+      this.tableLoading = true;
+      this.$api.getDepartmentList().then(res => {
+        this.tableLoading = false;
+        if (res.success) {
+          this.tableData = res.data;
+          this.parentOptions = res.data.map(item => {
+            return { label: item.name, value: item._id };
+          });
+        }
+      });
     },
   },
 };
